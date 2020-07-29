d92f7761e8cf04f647bcc59b6c599b859eab0ff7
adso023 <43103279+adso023@users.noreply.github.com>
2020-07-29 14:02:22 -0400

    Finished styling keyboard work on input and logic

--- lib/Views/KeyboardView.dart
+++ lib/Views/KeyboardView.dart
@@ -12,10 +12,83 @@ class _KeyboardViewState extends State<KeyboardView> {
     return Container(
       height: (MediaQuery.of(context).size.height / 2),
       width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.orange)
+      color: Color.fromRGBO(11, 57, 84, 1),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              createButton(1),
+              createButton(2),
+              createButton(3),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              createButton(4),
+              createButton(5),
+              createButton(6),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              createButton(7),
+              createButton(8),
+              createButton(9)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              createDecimal(),
+              createButton(0),
+              createBackspace(),
+            ],
+          ),
+        ],
       ),
-      child: FlatButton(onPressed: (){}, child: Text('World')),
     );
   }
+
+  Container createButton(int number) => Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: (MediaQuery.of(context).size.width / 3) - 10,
+    height: ((MediaQuery.of(context).size.height / 2) / 4) - 10,
+    decoration: BoxDecoration(
+      //color: Color.fromRGBO(10, 46, 54, 1),
+      borderRadius: BorderRadius.all(Radius.circular(5.0))
+    ),
+    child: Text(number.toString(), style: TextStyle(color: Colors.white, fontSize: 30),),
+  );
+
+  Container createDecimal() => Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: (MediaQuery.of(context).size.width / 3) - 10,
+    height: ((MediaQuery.of(context).size.height / 2) / 4) - 10,
+    decoration: BoxDecoration(
+        //color: Color.fromRGBO(10, 46, 54, 1),
+        borderRadius: BorderRadius.all(Radius.circular(5.0))
+    ),
+    child: Text('.', style: TextStyle(color: Colors.white, fontSize: 30),),
+  );
+
+  Container createBackspace() => Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.all(10),
+    margin: EdgeInsets.all(5),
+    width: (MediaQuery.of(context).size.width / 3) - 10,
+    height: ((MediaQuery.of(context).size.height / 2) / 4) - 10,
+    decoration: BoxDecoration(
+        //color: Color.fromRGBO(10, 46, 54, 1),
+        borderRadius: BorderRadius.all(Radius.circular(5.0))
+    ),
+    child: Text(String.fromCharCodes(Runes('\u232B')), style: TextStyle(color: Colors.white, fontSize: 30),),
+  );
 }
