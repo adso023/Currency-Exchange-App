21e9cbc86f0898b3f3ee7e3e144de82662f47d82
adso023 <43103279+adso023@users.noreply.github.com>
2020-07-30 22:42:19 -0400

    Containing the Country Lists and Exchange class to hold information

---
+++ lib/Data/CountryList.dart
@@ -0,0 +1,356 @@
+class CountryList {
+  List<Map<String, String>> _countries = [
+    {
+      "Code": "AED",
+      "Name": "UAE Dirham",
+      "Country": "United Arab Emirates"
+    },
+    {
+      "Code": "ARS",
+      "Name": "Argentine Peso",
+      "Country": "Argentina"
+    },
+    {
+      "Code": "AUD",
+      "Name": "Australian Dollar",
+      "Country": "Australia"
+    },
+    {
+      "Code": "BGN",
+      "Name": "Bulgarian Lev",
+      "Country": "Bulgaria"
+    },
+    {
+      "Code": "BRL",
+      "Name": "Brazilian Real",
+      "Country": "Brazil"
+    },
+    {
+      "Code": "BSD",
+      "Name": "Bahamian Dollar",
+      "Country": "Bahamas"
+    },
+    {
+      "Code": "CAD",
+      "Name": "Canadian Dollar",
+      "Country": "Canada"
+    },
+    {
+      "Code": "CHF",
+      "Name": "Swiss Franc",
+      "Country": "Switzerland"
+    },
+    {
+      "Code": "CLP",
+      "Name": "Chilean Peso",
+      "Country": "Chile"
+    },
+    {
+      "Code": "CNY",
+      "Name": "Chinese Renminbi",
+      "Country": "China"
+    },
+    {
+      "Code": "COP",
+      "Name": "Colombian Peso",
+      "Country": "Colombia"
+    },
+    {
+      "Code": "CZK",
+      "Name": "Czech Koruna",
+      "Country": "Czech Republic"
+    },
+    {
+      "Code": "DKK",
+      "Name": "Danish Krone",
+      "Country": "Denmark"
+    },
+    {
+      "Code": "DOP",
+      "Name": "Dominican Peso",
+      "Country": "Dominican Republic"
+    },
+    {
+      "Code": "EGP",
+      "Name": "Egyptian Pound",
+      "Country": "Egypt"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Germany"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Austria"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Belgium"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Cyprus"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Estonia"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Finland"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "France"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Greece"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Ireland"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Italy"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Latvia"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Lithuania"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Luxembourg"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Malta"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Netherlands"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Portugal"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Slovakia"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Slovenia"
+    },
+    {
+      "Code": "EUR",
+      "Name": "Euro",
+      "Country": "Spain"
+    },
+    {
+      "Code": "FJD",
+      "Name": "Fiji Dollar",
+      "Country": "Fiji"
+    },
+    {
+      "Code": "GBP",
+      "Name": "Pound Sterling",
+      "Country": "United Kingdom"
+    },
+    {
+      "Code": "GTQ",
+      "Name": "Guatemalan Quetzal",
+      "Country": "Guatemala"
+    },
+    {
+      "Code": "HKD",
+      "Name": "Hong Kong Dollar",
+      "Country": "Hong Kong"
+    },
+    {
+      "Code": "HRK",
+      "Name": "Croatian Kuna",
+      "Country": "Croatia"
+    },
+    {
+      "Code": "HUF",
+      "Name": "Hungarian Forint",
+      "Country": "Hungary"
+    },
+    {
+      "Code": "IDR",
+      "Name": "Indonesian Rupiah",
+      "Country": "Indonesia"
+    },
+    {
+      "Code": "ILS",
+      "Name": "Israeli New Shekel",
+      "Country": "Israel"
+    },
+    {
+      "Code": "INR",
+      "Name": "Indian Rupee",
+      "Country": "India"
+    },
+    {
+      "Code": "ISK",
+      "Name": "Icelandic Krona",
+      "Country": "Iceland"
+    },
+    {
+      "Code": "JPY",
+      "Name": "Japanese Yen",
+      "Country": "Japan"
+    },
+    {
+      "Code": "KRW",
+      "Name": "South Korean Won",
+      "Country": "South Korea"
+    },
+    {
+      "Code": "KZT",
+      "Name": "Kazakhstani Tenge",
+      "Country": "Kazakhstan"
+    },
+    {
+      "Code": "MVR",
+      "Name": "Maldivian Rufiyaa",
+      "Country": "Maldives"
+    },
+    {
+      "Code": "MXN",
+      "Name": "Mexican Peso",
+      "Country": "Mexico"
+    },
+    {
+      "Code": "MYR",
+      "Name": "Malaysian Ringgit",
+      "Country": "Malaysia"
+    },
+    {
+      "Code": "NOK",
+      "Name": "Norwegian Krone",
+      "Country": "Norway"
+    },
+    {
+      "Code": "NZD",
+      "Name": "New Zealand Dollar",
+      "Country": "New Zealand"
+    },
+    {
+      "Code": "PAB",
+      "Name": "Panamanian Balboa",
+      "Country": "Panama"
+    },
+    {
+      "Code": "PEN",
+      "Name": "Peruvian Sol",
+      "Country": "Peru"
+    },
+    {
+      "Code": "PHP",
+      "Name": "Philippine Peso",
+      "Country": "Philippines"
+    },
+    {
+      "Code": "PKR",
+      "Name": "Pakistani Rupee",
+      "Country": "Pakistan"
+    },
+    {
+      "Code": "PLN",
+      "Name": "Polish Zloty",
+      "Country": "Poland"
+    },
+    {
+      "Code": "PYG",
+      "Name": "Paraguayan Guarani",
+      "Country": "Paraguay"
+    },
+    {
+      "Code": "RON",
+      "Name": "Romanian Leu",
+      "Country": "Romania"
+    },
+    {
+      "Code": "RUB",
+      "Name": "Russian Ruble",
+      "Country": "Russia"
+    },
+    {
+      "Code": "SAR",
+      "Name": "Saudi Riyal",
+      "Country": "Saudi Arabia"
+    },
+    {
+      "Code": "SEK",
+      "Name": "Swedish Krona",
+      "Country": "Sweden"
+    },
+    {
+      "Code": "SGD",
+      "Name": "Singapore Dollar",
+      "Country": "Singapore"
+    },
+    {
+      "Code": "THB",
+      "Name": "Thai Baht",
+      "Country": "Thailand"
+    },
+    {
+      "Code": "TRY",
+      "Name": "Turkish Lira",
+      "Country": "Turkey"
+    },
+    {
+      "Code": "TWD",
+      "Name": "New Taiwan Dollar",
+      "Country": "Taiwan"
+    },
+    {
+      "Code": "UAH",
+      "Name": "Ukrainian Hryvnia",
+      "Country": "Ukraine"
+    },
+    {
+      "Code": "USD",
+      "Name": "United States Dollar",
+      "Country": "United States"
+    },
+    {
+      "Code": "UYU",
+      "Name": "Uruguayan Peso",
+      "Country": "Uruguay"
+    },
+    {
+      "Code": "ZAR",
+      "Name": "South African Rand",
+      "Country": "South Africa"
+    }
+  ];
+
+  get countries => _countries;
+}
---
+++ lib/Data/Exchange.dart
@@ -0,0 +1,25 @@
+class Exchange  {
+  final String _result;
+  final String _baseCode;
+  final List<Map<String, double>> _conversionRates;
+  final String _errorType;
+
+  Exchange({
+    String result,
+    String baseCode,
+    List<Map<String, double>> conversionRates,
+    String errorType}) :
+        _result = result,
+        _baseCode = baseCode,
+        _conversionRates = conversionRates,
+        _errorType = errorType;
+
+  factory Exchange.fromJson(Map<String, dynamic> json) {
+    return Exchange(
+      result: json["result"] ?? "error",
+      baseCode: json["base_code"] ?? "",
+      conversionRates: json["conversion_rates"] ?? [],
+      errorType: json["error-type"] ?? "NO ERROR"
+    );
+  }
+}
