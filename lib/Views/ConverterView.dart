5c472b4750fd3d5ec64ff2022e5aa07698cdc20c
adso023 <43103279+adso023@users.noreply.github.com>
2020-07-31 15:12:03 -0400

    Add reference to api class

--- lib/Views/ConverterView.dart
+++ lib/Views/ConverterView.dart
@@ -1,11 +1,14 @@
+import 'package:currencyexchange_app/Data/Exchange.dart';
+import 'package:currencyexchange_app/Data/ExchangeApi.dart';
 import 'package:flutter/material.dart';
 import 'package:currencyexchange_app/Views/CountryList.dart';
 
 class ConverterView extends StatefulWidget {
 
   final TextEditingController controller;
+  final ExchangeApi exchangeApi;
 
-  ConverterView({this.controller});
+  ConverterView({this.controller}) : exchangeApi = ExchangeApi();
 
   createState() => _ConverterViewState();
 }
@@ -14,6 +17,7 @@ class _ConverterViewState extends State<ConverterView> {
 
   Map<String, String> _from;
   Map<String, String> _to;
+  Exchange _exchange;
 
   @override
   void initState() {
@@ -22,6 +26,7 @@ class _ConverterViewState extends State<ConverterView> {
 
     _from = {"Code": "USD","Name": "United States Dollar","Country": "United States"};
     _to = {"Code": "CAD","Name": "Canadian Dollar","Country": "Canada"};
+    widget.exchangeApi.fetchExchange(_from["Code"]).then((value) => _exchange = value);
   }
 
   @override
@@ -43,32 +48,44 @@ class _ConverterViewState extends State<ConverterView> {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  FlatButton(
-                    onPressed: () async {
-                      await showModalBottomSheet(
-                        context: context,
-                        backgroundColor: Color(0xFF737373),
-                        builder: (context) => CountryListSheet(onCountrySelected: (country) {
-                          _from = country;
-                        },)
-                      );
+                  Material(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: FlatButton(
+                      onPressed: () async {
+                        await showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Color(0xFF737373),
+                          builder: (context) => CountryListSheet(onCountrySelected: (country) async{
+                            _from = country;
+                            _exchange = await widget.exchangeApi.fetchExchange(_from["Code"]);
+                            print(_exchange);
+                          },)
+                        );
 
-                      setState(() {print(_from);});
-                    },
-                    child: Text('${_from["Code"]}')
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: TextField(
-                      controller: widget.controller,
-                      showCursor: false,
-                      readOnly: true,
-                      autofocus: false,
-                      autocorrect: false,
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {
-
+                        setState(() {print(_from);});
                       },
+                      child: Text('${_from["Code"]}')
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                        controller: widget.controller,
+                        showCursor: false,
+                        readOnly: true,
+                        autofocus: false,
+                        autocorrect: false,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0)
+                        ),
+                        onChanged: (value) {
+
+                        },
+                      ),
                     ),
                   ),
                 ],
@@ -79,7 +96,9 @@ class _ConverterViewState extends State<ConverterView> {
             margin: EdgeInsets.all(10.0),
             child: GestureDetector(
               child: Icon(Icons.swap_vertical_circle, size: 48.0, color: Colors.indigo,),
-              onTap: (){},
+              onTap: (){
+                print('Hit the switcher button');
+              },
             )
           ),
           Material(
@@ -90,27 +109,40 @@ class _ConverterViewState extends State<ConverterView> {
               child: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  FlatButton(
-                    onPressed: () async{
-                      await showModalBottomSheet(
-                        context: context,
-                        backgroundColor: Color(0xFF737373),
-                        builder: (context) => CountryListSheet(onCountrySelected: (country) {
-                          _to = country;
-                        },)
-                      );
-                      setState(() {print(_to);});
-                    },
-                    child: Text('${_to["Code"]}')
+                  Material(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    color: Colors.orange,
+                    child: FlatButton(
+                      onPressed: () async{
+                        await showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Color(0xFF737373),
+                          builder: (context) => CountryListSheet(onCountrySelected: (country) {
+                            _to = country;
+                          },)
+                        );
+                        setState(() {print(_to);});
+                      },
+                      child: Text('${_to["Code"]}')
+                    ),
                   ),
-                  SizedBox(
-                    width: 200,
-                    child: TextField(
-                      showCursor: false,
-                      autofocus: false,
-                      autocorrect: false,
-                      readOnly: true,
-                      keyboardType: TextInputType.number,
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: SizedBox(
+                      width: 200,
+                      child: TextField(
+                        showCursor: false,
+                        autofocus: false,
+                        autocorrect: false,
+                        readOnly: true,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
+                        ),
+                        onChanged: (value) {
+
+                        },
+                      ),
                     ),
                   ),
                 ],
