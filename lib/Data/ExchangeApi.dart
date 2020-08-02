5049bc2a899f599e9664c6668fde46d4b82c3b79
adso023 <43103279+adso023@users.noreply.github.com>
2020-08-01 21:26:47 -0400

    Api usage to get currency exchange for base symbol

--- lib/Data/ExchangeApi.dart
+++ lib/Data/ExchangeApi.dart
@@ -3,15 +3,15 @@ import 'package:currencyexchange_app/Data/Exchange.dart';
 import 'package:http/http.dart' as http;
 
 class ExchangeApi {
-  String _apiKey = "237eed2856d135b894c7"; //Add your api key here
-  String _baseUrl = "https://free.currconv.com";
+//  String _apiKey = "f7a34529f20d7fe7b112"; //Add your api key here
+//  String _baseUrl = "https://free.currconv.com";
 
   Future<Exchange> fetchExchange(String code) async{
-    final response = await http.get('');
+    final response = await http.get('https://api.exchangeratesapi.io/latest?base=$code');
     if(response.statusCode == 200) {
       return Exchange.fromJson(json.decode(response.body));
     } else {
-      return Exchange(errorType: "unknown-code", result: "error");
+      return Exchange(error: "Something");
     }
   }
 }
